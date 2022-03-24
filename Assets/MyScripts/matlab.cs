c8e5f5bbf81783eba1e1bfe361edba550d51b59d
Tim Ding <ding@ucdavis.edu>
2022-03-24 14:13:41 -0400

    matlab

---
+++ Assets/MyScripts/matlab.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System;
+using System.IO;
+using System.Text;
+using static Monkey2D;
+public class readSocket : MonoBehaviour
+{
+    // Use this for initialization
+    TcpListener listener;
+    String msg;
+    void Start()
+    {
+        listener = new TcpListener(55001);
+        listener.Start();
+        print("is listening");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!listener.Pending())
+        {
+        }
+        else
+        {
+            print("socket comes");
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            msg = reader.ReadToEnd();
+            print(msg);
+        }
+    }
+}
