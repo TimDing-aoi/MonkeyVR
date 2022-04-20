ffaa2b839bb6c78c37c94e7861bac15b1e67448e
Tim Ding <ding@ucdavis.edu>
2022-04-20 14:03:07 -0400

    Revert "MicroSimu"
    This reverts commit 2feb4a22d75c28f287c23d5dd0ef29534b302d05.

--- Assets/Pupil/Scripts/CalibrationStatusText.cs
+++ Assets/Pupil/Scripts/CalibrationStatusText.cs
@@ -29,8 +29,6 @@ namespace PupilLabs
             calibrationController.OnCalibrationFailed += CalibrationFailed;
             calibrationController.OnFuseTestStarted += FuseTestStarted;
             calibrationController.OnFuseTestComplete += FuseTestComplete;
-            calibrationController.OnMicroSimuStarted += MicroSimuStarted;
-            calibrationController.OnMicroSimuComplete += MicroSimuComplete;
         }
 
         void OnDisable()
@@ -42,16 +40,12 @@ namespace PupilLabs
             calibrationController.OnCalibrationFailed -= CalibrationFailed;
             calibrationController.OnFuseTestStarted -= FuseTestStarted;
             calibrationController.OnFuseTestComplete -= FuseTestComplete;
-            calibrationController.OnMicroSimuStarted -= MicroSimuStarted;
-            calibrationController.OnMicroSimuComplete -= MicroSimuComplete;
         }
 
         private void OnConnected()
         {
             string text = "Connected";
-            text += "\n\nPlease warm up your eyes and press 'C' to start the calibration or 'P' to preview the calibration targets.\n\n" +
-                "You may also press 'N' to start the Firefly Task (only if calibration has already been done).\n" +
-                "Or, you may press 'F' to start the Fusing Test, or M for the micro simulation.";
+            text += "\n\nPlease warm up your eyes and press 'C' to start the calibration or 'P' to preview the calibration targets.\n\nYou may also press 'N' to start the Firefly Task (only if calibration has already been done).\nOr, you may press 'F' to start the Fusing Test.";
             SetStatusText(text);
         }
 
@@ -92,16 +86,6 @@ namespace PupilLabs
             SetStatusText("Fuse Test complete.");
 
             StartCoroutine(ChangeTextAfter(1.0f, "Press 'N' to start the Firefly Task.\nPress 'F' to start the Fusing Test."));
-        }
-
-        private void MicroSimuStarted()
-        {
-            SetStatusText("Micro Simulation in progress.");
-        }
-
-        private void MicroSimuComplete()
-        {
-            SetStatusText("Micro Simulation complete.");
         }
 
         private void SetStatusText(string text)
